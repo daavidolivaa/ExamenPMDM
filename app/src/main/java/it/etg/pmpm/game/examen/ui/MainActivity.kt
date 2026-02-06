4b112ce87900031163ad5e764278decea2e0b7c8
daavidolivaa <deividolivahuelamo@gmail.com>
2026-02-06 18:59:00 +0100

    2 commit

--- app/src/main/java/it/etg/pmpm/game/examen/ui/MainActivity.kt
+++ app/src/main/java/it/etg/pmpm/game/examen/ui/MainActivity.kt
@@ -1,21 +1,91 @@
 package it.etg.pmpm.game.examen.ui
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import it.etg.pmpm.game.examen.R
+import it.etg.pmpm.game.examen.api.RetrofitInstance
+import it.etg.pmpm.game.examen.database.AppDatabase
+import it.etg.pmpm.game.examen.database.GamesEntity
+import it.etg.pmpm.game.examen.databinding.ActivityMainBinding
+import it.etg.pmpm.game.examen.ui.adapter.GamesAdapter
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val NOMBRE_BD = "app_bd"
+        const val TAG = "ORIGEN"
+        const val MSG_REST = "Actualizando desde REST"
+        const val MSG_BD_VACIA = "BD vacia -> REST"
+        const val MSG_BD_DATOS = "BD con datos -> cargar BD"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bd : AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setRoom()
+        comprobarDatosIniciales()
+
+        binding.btnActualizar.setOnClickListener {
+            actualizarDesdeRest()
         }
+        }
+
+    private fun setRoom(){
+        bd = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, NOMBRE_BD
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+    private fun comprobarDatosIniciales(){
+        lifecycleScope.launch {
+            val datosBD = bd.gamesDao().getAllObjects()
+
+            if (datosBD.isEmpty()){
+                Log.d(TAG, MSG_BD_VACIA)
+                val datosApi = RetrofitInstance.api.getGames()
+
+                val entidades = datosApi.map {
+                    GamesEntity(it.id, it.title, it.genre, it.developer)
+                }
+                bd.gamesDao().insertAll(entidades)
+                mostrarEnListView(entidades)
+            } else{
+                Log.d(TAG, MSG_BD_DATOS)
+                mostrarEnListView(datosBD)
+            }
+        }
+    }
+
+    private fun actualizarDesdeRest(){
+        lifecycleScope.launch {
+            Log.d(TAG, MSG_REST)
+            bd.gamesDao().deleteAll()
+            val datosApi = RetrofitInstance.api.getGames()
+            val entidades = datosApi.map {
+                GamesEntity(it.id, it.title, it.genre, it.developer)
+            }
+            bd.gamesDao().insertAll(entidades)
+            mostrarEnListView(entidades)
+        }
+    }
+    private fun mostrarEnListView(lista: List<GamesEntity>){
+        val adapter = GamesAdapter(this, lista)
+        binding.listView.adapter = adapter
     }
 }
--- app/src/main/java/it/etg/pmpm/game/examen/ui/adapter/GamesAdapter.kt
+++ app/src/main/java/it/etg/pmpm/game/examen/ui/adapter/GamesAdapter.kt
@@ -1,21 +1,43 @@
 package it.etg.pmpm.game.examen.ui.adapter
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import it.etg.pmpm.game.examen.R
+import it.etg.pmpm.game.examen.database.GamesEntity
 
-class GamesAdapter : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.item_games)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class GamesAdapter(context : Context, private val lista:List<GamesEntity>)
+    : ArrayAdapter<GamesEntity>(context,CERO,lista) {
+
+        companion object{
+            const val CERO = 0
+        }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val view = convertView?: LayoutInflater.from(context).inflate(R.layout.item_games, parent,false)
+
+        val item = lista[position]
+
+        val txtId = view.findViewById<TextView>(R.id.txtId)
+        val txtTitle = view.findViewById<TextView>(R.id.txtTitle)
+        val txtGenre = view.findViewById<TextView>(R.id.txtGenre)
+        val txtDeveloper = view.findViewById<TextView>(R.id.txtDeveloper)
+
+        txtId.text =  "ID: ${item.id}"
+        txtTitle.text =  "Title: ${item.title}"
+        txtGenre.text =  "Genre: ${item.genre}"
+        txtDeveloper.text =  "Developer: ${item.developer}"
+
+        return view
+
         }
     }
-}
